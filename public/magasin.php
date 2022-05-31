eb29ca7e44c0a5c2f14ca0cafb25a4b972d9d865
Janick Bergeron <janick.bergeron392@outlook.fr>
2022-05-31 14:27:01 -0400

    magasin creation

---
+++ public/magasin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="search-bar">
+        
+        <input type="text" placeholder="Recherche..">
+
+    </div>
+
+    <div>
+    <label class="container">One
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+
+    <label class="container">Two
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+
+    <label class="container">Three
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+
+    <label class="container">Four
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+    </label>
+    </div>
+</body>
+</html>
